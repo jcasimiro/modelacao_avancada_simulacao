4147e7c1eade0b93f1ccbba6119cb16695e6172f
José Amílcar Ferreira Casimiro <jose.a.casimiro@gmail.com>
2023-03-14 22:21:32 +0000

    Cristian

--- 2023-03-14/caixa_de_ehnrenfest_2.py
+++ 2023-03-14/caixa_de_ehnrenfest_2.py
@@ -3,17 +3,17 @@ import random
 output = []
 #configuracao inicial
 output.append(0)
-p00 = 1/3
-p01 = 1/3
-p02 = 1/3
+p00 = 0.2
+p01 = 0.3
+p02 = 0.5
 
-p10 = 1/3
-p11 = 1/3
-p12 = 1/3
+p10 = 0.1
+p11 = 0.3
+p12 = 0.6
 
-p20 = 1/3
-p21 = 1/3
-p22 = 1/3
+p20 = 0.2
+p21 = 0.3
+p22 = 0.5
 
 n_elements = 100
 
@@ -23,26 +23,27 @@ for i in range(n_elements):
         if n > p00:
             output.append(0)
         else:   
-            n = random.random()-p00
+            n = random.uniform(0,(1-p00))
             if n > p01:
                 output.append(1)
             else:
                 output.append(2)
-                
+    n = random.random()            
     if output[i]==1:
         if n > p11:
             output.append(1)
         else:   
-            n = random.random()-p11
+            n = random.uniform(0,(1-p11))
             if n > p10:
                 output.append(0)
             else:
                 output.append(2)
+    n = random.random()
     if output[i]==2:
         if n > p22:
             output.append(2)
         else:   
-            n = random.random()-p22
+            n = random.uniform(0,(1-p22))
             if n > p20:
                 output.append(0)
             else:
